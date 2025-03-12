ec01aee2165f9e4aa77141bdb7b43f069456d302
Ramu0526 <danielmarkc2605@gmail.com>
2025-03-11 22:13:32 -0500

    Add files via upload

---
+++ Java_array/src/main/java/java_array/java_array.java
@@ -0,0 +1,17 @@
+package java_array;
+
+public class java_array {
+    
+    public static void main(String[] args) {
+       Lista objListadinamica=new Lista();
+       objListadinamica.mostrarCliente();
+       objListadinamica.Remover();
+       objListadinamica.Actualizar();
+       objListadinamica.Tamaño();
+       objListadinamica.Obtener();
+       objListadinamica.Contener();
+       objListadinamica.validarContenido();
+       objListadinamica.Ordenar();
+       objListadinamica.Limpiar();
+    }  
+}
